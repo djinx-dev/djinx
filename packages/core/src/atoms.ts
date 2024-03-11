71102f9c9e91af2cf15d18f616a3dde68cb0628d
Adrien <adrien@arccoza.com>
2024-03-11 19:22:20 +0800

    feat: add atom types and stringifyAtom fn

---
+++ packages/core/src/atoms.ts
@@ -0,0 +1,25 @@
+export type Atom = {
+  [key: string]: string | Atom
+}
+
+export type AtomGen = (k: string, v: string) => Atom
+
+export type Sheet = {
+  [key: string]: Atom
+}
+
+export function stringifyAtom(atom: Atom, tab="  ", tabDepth=0) {
+  const result = ["{"]
+  const pad = "".padEnd(tabDepth + 1, tab)
+
+  for (const [k, v] of Object.entries(atom)) {
+    if (typeof v !== "string") {
+      result.push(`${pad}${k}: ${stringifyAtom(v, tab, tabDepth + 1)}`)
+    } else {
+      result.push(`${pad}${k}: ${v};`)
+    }
+  }
+
+  result.push(`${"".padEnd(tabDepth, tab)}}`)
+  return result.join("\n")
+}
