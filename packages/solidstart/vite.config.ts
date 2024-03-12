d00c441979793f232782404fdb70fbabbe72b7a9
Adrien <adrien@arccoza.com>
2024-03-12 16:32:53 +0800

    build: update output file name and exports

--- packages/solidstart/vite.config.ts
+++ packages/solidstart/vite.config.ts
@@ -6,7 +6,13 @@ import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, "src/main.ts"), formats: ["es"] } },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+      fileName: "main",
+    },
+  },
   resolve: { alias: { src: resolve("src/") } },
   plugins: [dts(), solidPlugin()],
 })
