bf45b079a156f571b107e9bd74a98f8bbba3f0aa
Adrien <adrien@arccoza.com>
2024-05-16 12:21:00 +0800

    fix: split on whitespace with comma(s), make more flexible

--- packages/core/src/css.ts
+++ packages/core/src/css.ts
@@ -29,7 +29,7 @@ export function css(conf: Config, sheet: Record<string, Atom | null>, styles: Re
 }
 
 export function split(conf: Config, prop: string, decls: string): [string, string, AtomGen | undefined, string[]][] {
-  return decls.trim().split(/,+ */).map((decl) => {
+  return decls.trim().split(/\s*,+\s*/).map((decl) => {
     const [value="", modifiers] = decl.split("@")
     const selector = "." + cssEscape(`${prop}:${value}`)
     const atomGen = conf.generators[prop]
