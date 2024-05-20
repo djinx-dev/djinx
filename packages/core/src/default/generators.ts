5f9b862cc84755953e510869ea0db3f85074ba76
Adrien <adrien@arccoza.com>
2024-05-20 12:35:02 +0800

    chore: update pmbr generators and helper

--- packages/core/src/default/generators.ts
+++ packages/core/src/default/generators.ts
@@ -128,41 +128,27 @@ export const generators: Generators = {
     // 2 2 2 2 rel
     // 2 2 fix
     // 2
-
-    const prop = lookup[k]!
-    const vals = cv.split(/\s+/)
-    const ort = vals[vals.length - 1] === "rel" ? vals.pop()
-    return pmbr(prop, vals)
+    return pmbr(k, cv)
   },
 
   m(k: string, cv: string) {
-    const prop = lookup[k]!
-    const vals = cv.split(/\s+/)
-    return pmbr(prop, vals)
+    return pmbr(k, cv)
   },
 
   bw(k: string, cv: string) {
-    const prop = lookup[k]!
-    const vals = cv.split(/\s+/)
-    return pmbr(prop, vals, "width")
+    return pmbr(k, cv, "width")
   },
 
   bc(k: string, cv: string) {
-    const prop = lookup[k]!
-    const vals = cv.split(/\s+/)
-    return pmbr(prop, vals, "color")
+    return pmbr(k, cv, "color")
   },
 
   bs(k: string, cv: string) {
-    const prop = lookup[k]!
-    const vals = cv.split(/\s+/)
-    return pmbr(prop, vals, "style")
+    return pmbr(k, cv, "style")
   },
 
   r(k: string, cv: string) {
-    const prop = lookup[k]!
-    const vals = cv.split(/\s+/)
-    return pmbr(prop, vals, "radius")
+    return pmbr(k, cv, "radius")
   },
 
   s(k: string, cv: string) {
@@ -170,30 +156,55 @@ export const generators: Generators = {
   },
 }
 
-function pmbr (prop: string, vals: string[], suffix="") {
+
+// *** Util functions and objects ***
+
+function pmbr (k: string, cv: string, suffix="") {
+  const prop = lookup[k]!
+  let vals = cv.split(/\s+/) as (string | undefined)[]
+  const tail = vals[vals.length - 1]
+  let mode =  tail === "rel" || tail === "fix" ? vals.pop() : "fix"
   const atom: Atom = {}
+  mode = suffix === "radius" ? `${mode}-${suffix}` : mode
   suffix = suffix ? `-${suffix}` : ""
 
   switch (vals.length) {
   case 1:
-    !isEmpty(vals[0]) && (atom[prop] = defUnits(vals[0]) || "")
+    vals = [vals[0], undefined, vals[0], undefined]
     break
   case 2:
-    !isEmpty(vals[0]) && (atom[`${prop}-inline${suffix}`] = defUnits(vals[0]) || "")
-    !isEmpty(vals[1]) && (atom[`${prop}-block${suffix}`] = defUnits(vals[1]) || "")
+    vals = [vals[0], vals[1], vals[0], vals[1]]
     break
-  case 4:
-    !isEmpty(vals[0]) && (atom[`${prop}-block-start${suffix}`] = defUnits(vals[0]) || "")
-    !isEmpty(vals[1]) && (atom[`${prop}-inline-end${suffix}`] = defUnits(vals[1]) || "")
-    !isEmpty(vals[2]) && (atom[`${prop}-block-end${suffix}`] = defUnits(vals[2]) || "")
-    !isEmpty(vals[3]) && (atom[`${prop}-inline-start${suffix}`] = defUnits(vals[3]) || "")
-    break
+  }
+
+  if (vals.length == 4) {
+    !isEmpty(vals[0]) && (atom[`${prop}-${pmbrMap[`a-${mode}`]}${suffix}`] = defUnits(vals[0]) || "")
+    !isEmpty(vals[1]) && (atom[`${prop}-${pmbrMap[`b-${mode}`]}${suffix}`] = defUnits(vals[1]) || "")
+    !isEmpty(vals[2]) && (atom[`${prop}-${pmbrMap[`c-${mode}`]}${suffix}`] = defUnits(vals[2]) || "")
+    !isEmpty(vals[3]) && (atom[`${prop}-${pmbrMap[`d-${mode}`]}${suffix}`] = defUnits(vals[3]) || "")
   }
 
   return atom
 }
 
-// *** Util functions and objects ***
+const pmbrMap = {
+  "a-fix": "top",
+  "b-fix": "right",
+  "c-fix": "bottom",
+  "d-fix": "left",
+  "a-rel": "block-start",
+  "b-rel": "inline-end",
+  "c-rel": "block-end",
+  "d-rel": "inline-start",
+  "a-fix-radius": "top-left",
+  "b-fix-radius": "top-right",
+  "c-fix-radius": "bottom-right",
+  "d-fix-radius": "bottom-left",
+  "a-rel-radius": "start-start",
+  "b-rel-radius": "start-end",
+  "c-rel-radius": "end-end",
+  "d-rel-radius": "end-start",
+}
 
 function isEmpty(v: any) {
   return v == null || v == "" || v == "_"
